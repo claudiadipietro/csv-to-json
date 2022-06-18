a0110a4e14cab59c9fa542222abe9765f3f1ad64
Claudia Di Pietro <claudiadipietro12@gmail.com>
2022-06-18 15:17:34 +0200

    App moved to its own file

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
-const express = require('express');
-const req = require('express/lib/request');
-const { json } = require('express/lib/response');
-const res = require('express/lib/response');
-const csvtojson = require('csvtojson');
-const parse = require('csv-parser');
-const fs = require('fs');
-const app = express();
-const csvDecoded = [];
-const type = 'base64'
-
-app.use(express.json());
-
-app.post('/api/csv', (req, res) =>{
-  const base64csv = req.body.base64csv
-  const base64decoded = new Buffer.from(base64csv, type).toString("ascii");
-  console.log(base64decoded)
-  csvtojson()
-  .fromString(base64decoded)
-  .then((json) => {
-    res.send(json);
-  });
-});
-
+import app from './app.js';
 
 const port = process.env.port || 80;
-app.listen(port, () => console.log("Listening"));
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+});
