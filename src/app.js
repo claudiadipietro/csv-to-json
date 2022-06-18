4ac9b24b1662abbe9735a88111eefe5e0f29c8cc
Claudia Di Pietro <claudiadipietro12@gmail.com>
2022-06-18 16:32:57 +0200

    Endpoint finished and tested

--- src/app.js
+++ src/app.js
@@ -4,17 +4,38 @@ import csvtojson from 'csvtojson';
 const app = express();
 app.use(express.json());
 
-const type = 'base64'
-
 app.post('/api/csv', (request, response) =>{
-  const base64File = request.body.base64File
-  const base64Buffer = new Buffer.from(base64File, type);
-  const base64Decoded = base64Buffer.toString('ascii');
-  csvtojson()
-  .fromString(base64Decoded)
-  .then((json) => {
-    response.send(json);
+  const base64Decoded = getFileDecoded(request);
+  if (!base64Decoded) {
+    return response.status(200).send(['No file uploaded']);
+  }
+  csvtojson().fromString(base64Decoded).then((json) => {
+    console.log(json)
+    const jsonArray = convertNumericStringsToNumbers(json);
+    return response.send(jsonArray);
   });
 });
+
+function getFileDecoded (request) {
+  const base64File = request.body?.base64File
+  if (!base64File) {
+    return false;
+  }
+  const base64Buffer = new Buffer.from(base64File, 'base64');
+  const base64Decoded = base64Buffer.toString('ascii');
+  return base64Decoded;
+}
+
+function convertNumericStringsToNumbers (jsonArray) {
+  for (var jsonObjectIndex in jsonArray) {
+    const jsonObject = jsonArray[jsonObjectIndex];
+    for (var property in jsonObject) {
+        if (Number(jsonObject[property])){
+          jsonArray[jsonObjectIndex][property] = Number(jsonObject[property]);
+        }
+    }
+  }
+  return jsonArray;
+}
 
 export default app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,38 @@
-const app = require('../src/index.js') // Link to your server file
-const supertest = require('supertest')
-const request = supertest(app)
+import app from '../src/app.js';
+import request from 'supertest';
+import fs from 'fs';
 
-it('gets the test endpoint', async done => {
-    const response = await request.get('/test')
-    expect(response.status).toBe(200)
-    expect(response.body.message).toBe('pass!')
-    done()
+describe('CSV Endpoint', () => {  
+  test('Should return an array', async () => {
+    await request(app).post('/api/csv')
+    .expect(200)
+    .expect(response => {
+      expect(response.body).toBeInstanceOf(Array);
+    });
+  });
+  test('Should return a message when no csv was uploaded', async () => {
+    await request(app).post('/api/csv').send({})
+    .expect(200)
+    .expect(response => {
+      expect(response.body).toEqual(['No file uploaded']);
+    });
+  });
+  test('Should return the data from the csv uploaded', async () => {
+    const base64 = fs.readFileSync('tests' + '/test_file.csv', 'base64');
+    await request(app).post('/api/csv').send({ base64File: `${base64}` })
+    .expect(200)
+    .expect(response => {
+      expect(response.body).toEqual([{
+        "test": "yes",
+        "name": "Claudia",
+        "last_name": "Di Pietro",
+        "age": 21
+      },{
+        "test": "yes",
+        "name": "Andrea",
+        "last_name": "Covian",
+        "age": 21
+      }]);
+    });
+  });
 })
